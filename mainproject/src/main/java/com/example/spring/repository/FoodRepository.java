a9edad91ac3620bf329df495842fefe2a71c3d2b
Giridharlal <103944702+Giridharlal@users.noreply.github.com>
2022-07-03 21:32:56 +0530

    Add files via upload

---
+++ mainproject/src/main/java/com/example/spring/repository/FoodRepository.java
@@ -0,0 +1,14 @@
+package com.example.spring.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.spring.entity.Food;
+
+public interface FoodRepository extends JpaRepository<Food, Integer> {
+	
+	public List<Food> findAllByName(String name);
+	//public Food findByFoodId(int foodId);
+	public List<Food> findAllByCategory(String category);
+
+	
+}
